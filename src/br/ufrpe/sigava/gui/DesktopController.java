f194f52f2ddd0b4e8411a261916f92220a920413
elivelton-pc <elivelton.rodrigues@ufrpe.br>
2018-07-19 16:47:15 -0300

    Att pagina inicial

--- src/br/ufrpe/sigava/gui/DesktopController.java
+++ src/br/ufrpe/sigava/gui/DesktopController.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,38 +8,8 @@ package br.ufrpe.sigava.gui;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-
-/**
- * FXML Controller class
- *
- * @author elive
- */
-public class DesktopController implements Initializable {
-
-
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
-}
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package br.ufrpe.sigava.gui;
-
-import java.net.URL;
-import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -53,19 +23,26 @@ import javafx.scene.control.ComboBox;
 public class DesktopController implements Initializable {
 
     @FXML
-    private ComboBox<?> box_aluno;
+    private Button btn_Aluno;
     @FXML
-    private ComboBox<?> box_disc;
+    private Button btn_Professor;
     @FXML
-    private ComboBox<?> box_prof;
+    private Button btn_Disciplina;
+
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
+        btn_Aluno.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        });
+        
     }    
     
 }
->>>>>>> master

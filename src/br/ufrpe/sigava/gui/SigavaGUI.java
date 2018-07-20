256dd677741de512c03bd1a3daa9b688054de629
thiagocossta <thiagocossta_@hotmail.com>
2018-07-20 16:39:53 -0300

    Att GUI

--- src/br/ufrpe/sigava/gui/SigavaGUI.java
+++ src/br/ufrpe/sigava/gui/SigavaGUI.java
@@ -19,7 +19,7 @@ public class SigavaGUI extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Desktop.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
         
         Scene scene = new Scene(root);
         
